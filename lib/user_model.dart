7ed9b967a69ee114eb60041b283f735f7cf5539e
Vainyala17 <samalvainyala174@gmail.com>
2025-07-25 01:19:54 +0530

    first commit

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -1,10 +1,12 @@
-
 class UserModel {
   final String id;
   final String name;
   final String email;
   final String photo;
   final String loginType;
+  final String? dob;
+  final String? gender;
+  final String? phone;
 
   UserModel({
     required this.id,
@@ -12,25 +14,30 @@ class UserModel {
     required this.email,
     required this.photo,
     required this.loginType,
+    this.dob,
+    this.gender,
+    this.phone,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-      'photo': photo,
-      'loginType': loginType,
-    };
-  }
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+    'email': email,
+    'photo': photo,
+    'loginType': loginType,
+    'dob': dob,
+    'gender': gender,
+    'phone': phone,
+  };
 
-  factory UserModel.fromMap(Map<String, dynamic> map) {
-    return UserModel(
-      id: map['id'] ?? '',
-      name: map['name'] ?? '',
-      email: map['email'] ?? '',
-      photo: map['photo'] ?? '',
-      loginType: map['loginType'] ?? '',
-    );
-  }
+  factory UserModel.fromMap(Map<String, dynamic> map) => UserModel(
+    id: map['id'],
+    name: map['name'],
+    email: map['email'],
+    photo: map['photo'],
+    loginType: map['loginType'],
+    dob: map['dob'],
+    gender: map['gender'],
+    phone: map['phone'],
+  );
 }
